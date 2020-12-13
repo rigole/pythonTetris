04264d7fb6f99826bb57d6c760e0459043fcf683
rigole <foplacide@gmail.com>
2020-12-13 22:49:16 +0100

    next figure suite

--- main.py
+++ main.py
@@ -215,7 +215,28 @@ def draw_grid(surface, grid):
 
 
 def clear_rows(grid, locked):
-    pass
+    inc = 0
+    for i in range(len(grid)-1, -1, -1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            nd = i
+
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+
+                    continue
+#if inc > 0:
+       # for key in sorted(list(locked), key = lambda x: x[1])[::-1]:
+
+
+
+
+
+
+
 
 
 
