01c92540a53d54fe586a85ae3fee78b443f9d414
rigole <foplacide@gmail.com>
2020-12-14 01:47:35 +0100

    losing display and function

--- main.py
+++ main.py
@@ -201,7 +201,10 @@ def get_shape():
 
 
 def draw_text_middle(text, size, color, surface):
-    pass
+    font = pygame.font.SysFont("comicsans", size, bold=True)
+    label = font.render(text, 1, color)
+
+    surface.blit(label, (top_left_x + play_width /2 - (label.get_width()/2), top_left_y + play_height/2 - label.get_height()/2))
 
 
 def draw_grid(surface, grid):
@@ -375,13 +378,28 @@ def main(win):
         pygame.display.update()
 
         if check_lost(locked_positions):
+             draw_text_middle("Vous avez Perdu", 80, (255, 255, 255),win)
+             pygame.display.update()
+             pygame.time.delay(1500)
              run = False
 
     pygame.display.quit()
 
 
 def main_menu(win):
-    main(win)
+    run = True
+    while run:
+        win.fill((0, 0 , 0))
+        draw_text_middle("Appuyez sur n'importe quelle touche ", 60, (255, 255, 255))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                main()
+
+
+    pygame.display.quit()
 
 
 win = pygame.display.set_mode((s_width, s_height))
